97c73501d9b6acae2d36805425a2bea5696ce94c
JhonHernandez <jhonhernandez300@gmail.com>
2021-06-03 07:55:49 -0500

    inicio2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { VerhorasComponent } from './verhoras/verhoras.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ingresarservicio/ingresarservicio.component.ts
+++ src/app/ingresarservicio/ingresarservicio.component.ts
@@ -25,32 +25,66 @@ export class IngresarservicioComponent implements OnInit {
   };
   
   myForm: FormGroup;    
+  submitted = false;  
 
-   constructor(private formBuilder: FormBuilder, private dataService: DataService)  {          
+  constructor(private formBuilder: FormBuilder, private dataService: DataService)  {    }
+  //constructor(private dataService: DataService)  {    }
+iniciarFormulario(){
+  this.myForm = new FormGroup({
+    //tecnico: new FormControl(this.servicioSettings.tecnico, [Validators.required, Validators.minLength(4), Validators.maxLength(30)]),
+    tecnico: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(30)]),
+    servicioRealizado: new FormControl(this.servicioSettings.servicioRealizado, [Validators.required, Validators.minLength(4), Validators.maxLength(30)]),
+    fechaDeInicio: new FormControl(this.servicioSettings.fechaDeInicio, [Validators.required]),
+    horaDeInicio: new FormControl(this.servicioSettings.horaDeInicio, [Validators.required, Validators.maxLength(2)]),
+    fechaDeFinalizacion: new FormControl(this.servicioSettings.fechaDeFinalizacion, [Validators.required]),
+    horaDeFinalizacion: new FormControl(this.servicioSettings.horaDeFinalizacion, [Validators.required, Validators.maxLength(2)])    
+  });
+}
 
-      this.myForm = this.formBuilder.group({
-        tecnico: [this.servicioSettings.tecnico, Validators.required],
-        servicioRealizado: [this.servicioSettings.servicioRealizado, Validators.required],
-        fechaDeInicio: [this.servicioSettings.fechaDeInicio, Validators.required],
-        horaDeInicio: [this.servicioSettings.horaDeInicio, Validators.required],
-        fechaDeFinalizacion: [this.servicioSettings.fechaDeFinalizacion, Validators.required],
-        horaDeFinalizacion: [this.servicioSettings.horaDeFinalizacion, Validators.required]
-      });
-     
-   }
+public get tecnico(): FormGroup {
+  return this.myForm.get('tecnico') as FormGroup;
+}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.iniciarFormulario();    
   }
+
+  get f() { return this.myForm.controls }
 
   onSubmit() {
-    if (this.myForm.valid) {
-        console.log(this.myForm.value)
-        this.dataService.postServicioSettingsForm(this.myForm.value).subscribe(
-            result => console.log('success ', result),
-            error => console.log('error ', error)
-        );
+    this.submitted = true;
+
+    if (this.myForm.invalid) {
+      return      
     }
+    alert ('Datos guardados')
   }
+
+  //  constructor(private formBuilder: FormBuilder, private dataService: DataService)  {          
+
+  //     this.myForm = this.formBuilder.group({
+  //       tecnico: [this.servicioSettings.tecnico, Validators.required],
+  //       servicioRealizado: [this.servicioSettings.servicioRealizado, Validators.required],
+  //       fechaDeInicio: [this.servicioSettings.fechaDeInicio, Validators.required],
+  //       horaDeInicio: [this.servicioSettings.horaDeInicio, Validators.required],
+  //       fechaDeFinalizacion: [this.servicioSettings.fechaDeFinalizacion, Validators.required],
+  //       horaDeFinalizacion: [this.servicioSettings.horaDeFinalizacion, Validators.required]
+  //     });
+     
+  //  }
+
+  // ngOnInit(): void {
+  // }
+
+  // onSubmit() {
+  //   if (this.myForm.valid) {
+  //       console.log(this.myForm.value)
+  //       this.dataService.postServicioSettingsForm(this.myForm.value).subscribe(
+  //           result => console.log('success ', result),
+  //           error => console.log('error ', error)
+  //       );
+  //   }
+  // }
 
 
 }
